edfa22720dc8790660108d6474af37f615378a6c
Phillip Wang <kip.phil@gmail.com>
2019-09-30 05:08:35 -0500

    Remove motion sickness, interpolate correctly

--- Assets/Digger.cs
+++ Assets/Digger.cs
@@ -7,26 +7,58 @@ public class Digger : MonoBehaviour
     public GameObject headObject;
     public GameObject groundObject;
     private GameObject shovelObject;
+    private Terrain terrain;
     private bool isUnderground;
     private const double groundThreshold = 0.2;
+    private float contactX;
+    private float contactZ;
+
+    private float[,] mask = new float[5, 5] { { 0.95f, 0.96f, 0.98f, 0.96f, 0.95f }, { 0.96f, 0.98f, 0.99f, 0.98f, 0.96f }, { 0.98f, 0.99f, 1f, 0.99f, 0.98f }, { 0.96f, 0.98f, 0.99f, 0.98f, 0.96f }, { 0.95f, 0.96f, 0.98f, 0.96f, 0.95f } }; 
 
     // Start is called before the first frame update
     void Start()
     {
         shovelObject = this.gameObject;
         isUnderground = false;
+        terrain = groundObject.GetComponent<Terrain>();
+
+        float[,] heights = new float[100, 100];
+        for (int i=0; i<100; i++)
+        {
+            for (int j=0; j<100; j++)
+            {
+                heights[i, j] = 1f;
+            }
+        }
+        terrain.terrainData.SetHeights(0, 0, heights);
     }
 
     // Update is called once per frame
     void Update()
     {
-        double currentY = shovelObject.transform.localPosition.y + headObject.transform.localPosition.y;
-        if (isUnderground && currentY >= groundObject.transform.localPosition.y)
+        Vector3 currentPos = shovelObject.transform.localPosition + headObject.transform.localPosition;
+        float currentHeight = terrain.terrainData.GetHeight((int)currentPos.x, (int)currentPos.z);
+        if (isUnderground && currentPos.y >= currentHeight + groundObject.transform.localPosition.y)
         {
             isUnderground = false;
-            groundObject.transform.SetPositionAndRotation(groundObject.transform.localPosition + Vector3.down, groundObject.transform.localRotation);
+
+            int xCoor = (int)Mathf.Max(0, currentPos.x - 5);
+            int yCoor = (int)Mathf.Max(0, currentPos.z - 5);
+            float[,] heights = terrain.terrainData.GetHeights(xCoor, yCoor, 5, 5);
+            float[,] newHeightFloats = new float[5, 5];
+            Debug.Log(terrain.terrainData.heightmapScale.y);
+            Debug.Log("Printing the heights now");
+            for (int i=0; i<5; i++)
+            {
+                for (int j=0; j<5; j++)
+                {
+                    Debug.Log(heights[i, j]);
+                    newHeightFloats[i, j] = heights[i, j] - (mask[i, j] / 600f);
+                }
+            }
+            terrain.terrainData.SetHeights(xCoor, yCoor, newHeightFloats);
         }
-        else if (!isUnderground && currentY < groundObject.transform.localPosition.y - groundThreshold)
+        else if (!isUnderground && currentPos.y < currentHeight + groundObject.transform.localPosition.y - groundThreshold)
         {
             isUnderground = true;
         }
---
+++ Assets/IgnoreCollisions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnoreCollisions : MonoBehaviour
+{
+    public GameObject playerObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics.IgnoreCollision(this.GetComponent<Collider>(), playerObject.GetComponent<CharacterController>());
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Physics.IgnoreCollision(this.GetComponent<Collider>(), playerObject.GetComponent<CharacterController>());
+        Debug.Log(collision.gameObject.name);
+    }
+}
